28df6e31d0c3f413b8294f72cd78999fa2c71d8b
Rostyslav Harasymiak <rharasymiak@icstars.org>
2024-07-09 20:20:47 -0500

    JS GEOLOCATION AND Weather API CAll

--- script.js
+++ script.js
@@ -26,3 +26,49 @@ function showSlides(n) {
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
 }
+// Geolocation
+const x = document.getElementById("demo");
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else { 
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+  x.innerHTML = "Latitude: " + position.coords.latitude + 
+  "<br>Longitude: " + position.coords.longitude;
+}
+
+// Weather
+console.log("script loaded");
+let user_location = document.getElementById('location');
+let search = document.getElementById('search');
+let weather = document.getElementById('weather');
+const API_KEY = '75f410cee2ebf850961967e02bc5d2ac';
+
+function getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition);
+    } else { 
+      x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+  }
+
+search.addEventListener('click', function () {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${user_location.value}&appid=${API_KEY}`)
+        .then(response => response.json())
+        .then(data => {
+            let currentWeather = data.weather[0].description;
+            let currentTemp = Math.round(data.main.temp - 273.15);
+            weather.innerHTML = `
+                <p>Current weather: ${currentWeather}</p>
+                <p>Current temperature: ${currentTemp}°C</p>
+            `;
+        })
+        .catch(error => {
+            weather.innerHTML = `<p>Error: ${error}</p>`;
+        });
+});
